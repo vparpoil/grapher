2fee83bab582a1a65f5c2cbc66818c5c962f0700
Victor Parpoil <vparpoil@gmail.com>
2025-01-20 10:47:03 +0100

    fix: scoping of publication break classical Meteor.publish
    fixes https://github.com/cult-of-coders/grapher/pull/484#issuecomment-2600888015

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cultofcoders:grapher',
-  version: '2.0.0-beta.0',
+  version: '2.0.0-beta.1',
   // Brief, one-line summary of the package.
   summary: 'Grapher is a data fetching layer on top of Meteor',
   // URL to the Git repository containing the source code for this package.
